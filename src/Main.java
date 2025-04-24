3401f5a202c4b67a20e2eff71a74fa8ec061c416
Marion Rossier <marion.rossier2@gmail.com>
2025-04-24 10:17:59 +0200

    refactor: Move application startup logic to Spotify class for better organization

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
-import player_commandPattern.SpotifyPlayer;
-import player_commandPattern.commands.ICommand;
-import player_commandPattern.receivers.SpotifyService;
-import view_templatePattern.*;
-
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
 
-        // Initialisation des services nécessaires
-        SpotifyService spotifyService = new SpotifyService();
+        Spotify.startApp();
 
-        // Création de l'instance SpotifyPlayer
-        SpotifyPlayer spotifyPlayer = new SpotifyPlayer(
-                spotifyService,
-                new Stack<ICommand>());
-
-        // Création de l'instance SpotifyPageFactory avec SpotifyPlayer
-        SpotifyPageFactory miniSpotify = new SpotifyPageFactory();
-        miniSpotify.spotifyPlayer = spotifyPlayer; // Initialisation de SpotifyPlayer dans la factory
-
-        // Configuration des pages
-        miniSpotify.setUpPages();
-
-        // Démarrage de l'application
-        miniSpotify.startLogin();
     }
 }
